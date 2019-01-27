85e0a014af5bb5ddfbdfbeb7a280ea86e7b68aeb
tuxzz <dorazzsoft@gmail.com>
2019-01-27 15:27:06 +0800

    add some useful functions for lfmodel

--- test_lfmodel.py
+++ test_lfmodel.py
@@ -3,6 +3,20 @@ import pylab as pl
 from revoice import *
 from revoice.common import *
 
+sr = 48000
+nSample = int(3.0 * sr)
+T0 = 1.0 / 220.0
+T1 = 1.0 / 440.0
+Rd0 = 3 * (1 - 0.5)
+Rd1 = 3 * (1 - 0.8)
+
+print("Generializing constant...")
+saveWav("lf_variant.wav", lfmodel.calcVariantFlowDerivative(nSample, [0], [T0], [1.0], [Rd0], sr), sr)
+
+print("Generializing variant...")
+saveWav("lf_variant.wav", lfmodel.calcVariantFlowDerivative(nSample, [0, sr], [T0, T1], [1.0, 0.5], [Rd0, Rd1], sr), sr)
+
+print("Plotting...")
 t = np.linspace(0, 0.008, 16384)
 f = np.linspace(1.0, 4000.0, 16384 * 16)
 T0 = 0.008
