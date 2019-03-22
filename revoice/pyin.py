c2364ecc7eb540ab9d78d0bf75a1da0fcb57e6b2
otonashi1991 <xir.hikaria@gmail.com>
2019-03-22 14:29:25 +0800

    use fp32 to save memory

--- revoice/pyin.py
+++ revoice/pyin.py
@@ -1,23 +1,82 @@
 from .common import *
 from . import yin
 
+@nb.jit(nb.float32[:](nb.float32, nb.float32, nb.float32, nb.float32, nb.int64))
 def normalized_pdf(a, b, begin, end, number):
-    x = np.arange(0, number, dtype = np.float64) * ((end - begin) / number)
+    x = np.arange(0, number, dtype=np.float32) * ((end - begin) / number)
     v = np.power(x, a - 1.0) * np.power(1.0 - x, b - 1.0)
     for i in reversed(range(number - 1)):
         if(v[i] < v[i + 1]):
             v[i] = v[i + 1]
     return v / np.sum(v)
 
+@nb.jit(nb.float32[:](nb.float32[:]))
 def extractF0(obsProbList):
     nHop = len(obsProbList)
 
-    out = np.zeros(nHop, dtype = np.float64)
+    out = np.zeros(nHop, dtype=np.float32)
     for iHop, (freqProb) in enumerate(obsProbList):
-        if(len(freqProb) > 0):
+        if len(freqProb) > 0:
             out[iHop] = freqProb.T[0][np.argmax(freqProb.T[1])]
 
     return out
+
+@nb.jit(fastmath=True, nopython=True, cache=True)
+def _valleyToFreqProb(valleyIndexList, buff, outFreqProb, pdf, bias, probThreshold, weightPrior, sr):
+    (nValley,) = valleyIndexList.shape
+    (pdfSize,) = pdf.shape
+    freqProb = outFreqProb
+    probTotal = 0.0
+    weightedProbTotal = 0.0
+    for iValley, valley in enumerate(valleyIndexList):
+        ipledIdx, ipledVal = parabolicInterpolate(buff, valley, False)
+        freq = sr / ipledIdx
+        v0 = 1 if(iValley == 0) else min(1.0, max(0.0, buff[valleyIndexList[iValley - 1]]) + eps)
+        v1 = 0 if(iValley == nValley - 1) else min(1.0, max(0.0, buff[valleyIndexList[iValley + 1]]) + eps)
+        prob = 0.0
+        for i in range(int(v1 * pdfSize), int(v0 * pdfSize)):
+            prob += pdf[i] * (1.0 if(ipledVal < i / pdfSize) else 0.01)
+        prob = min(prob, 0.99)
+        prob *= bias
+        probTotal += prob
+        if(ipledVal < probThreshold):
+            prob *= weightPrior
+        weightedProbTotal += prob
+        freqProb[iValley] = freq, prob
+    return (probTotal, weightedProbTotal)
+
+def _pyinCore(x: np.ndarray, pdf: np.ndarray, bias: float, probThreshold: float, weightPrior: float, hopSize: float, valleyThreshold: float, valleyStep: float, minFreq: float, maxFreq: float, minWindowSize: float, windowLengthFac: float, sr: float, maxIter: int, removeDC: bool, out: np.ndarray) -> np.ndarray:
+    (nHop, maxFreqProbCount, _) = out.shape
+    
+    for iHop in range(nHop):
+        iCenter = int(round(iHop * hopSize))
+        windowSize = int(max(sr / minFreq * 4, minWindowSize) * windowLengthFac)
+        if(windowSize % 2 == 1):
+            windowSize += 1
+        for _ in range(maxIter):
+            frame = getFrame(x, iCenter, windowSize)
+            if removeDC:
+                frame = removeDCSimple(frame)
+            buff = yin.difference(frame)
+            yin.cumulativeDifference(buff)
+            valleyIndexList = np.asarray(yin.findValleys(buff, minFreq, maxFreq, sr, threshold = valleyThreshold, step = valleyStep, limit = maxFreqProbCount), dtype = np.int)
+            nValley = len(valleyIndexList)
+            if nValley > 0:
+                possibleFreq = min(maxFreq, max(sr / valleyIndexList[-1] - 20.0, minFreq))
+                newWindowSize = int(max(sr / possibleFreq * 4, minWindowSize) * windowLengthFac)
+                if newWindowSize % 2 == 1:
+                    newWindowSize += 1
+                if newWindowSize == windowSize:
+                    break
+                windowSize = newWindowSize
+        freqProb = np.zeros((nValley, 2), dtype=np.float32)
+        (probTotal, weightedProbTotal) = _valleyToFreqProb(valleyIndexList, buff, freqProb, pdf, bias, probThreshold, weightPrior, sr)
+
+        # renormalize
+        if nValley > 0 and weightedProbTotal != 0.0:
+            freqProb.T[1] *= probTotal / weightedProbTotal
+        
+        out[iHop,:nValley] = freqProb
 
 class Analyzer:
     def __init__(self, sr, **kwargs):
@@ -40,64 +99,26 @@ class Analyzer:
         self.removeDC = kwargs.get("removeDC", True)
         self.windowLengthFac = kwargs.get("windowLengthFac", 1.0)
         self.minWindowSize = kwargs.get("minWindowSize", self.hopSize * 3)
+        self.maxFreqProbCount = kwargs.get("maxFreqProbCount", 16)
+        self.useAccelerator = kwargs.get("useAccelerator", True)
+
+        self.pdf = np.require(self.pdf, dtype=np.float32, requirements="C")
 
     def __call__(self, x):
         (nX,) = x.shape
         nHop = getNFrame(nX, self.hopSize)
-        (pdfSize,) = self.pdf.shape
 
-        if(self.prefilter):
+        if self.prefilter:
             x = yin.applyPrefilter(x, self.maxFreq, self.samprate)
-
-        out = []
-
-        for iHop in range(nHop):
-            iCenter = int(round(iHop * self.hopSize))
-            windowSize = 0
-            newWindowSize = int(max(self.samprate / self.minFreq * 4, self.minWindowSize) * self.windowLengthFac)
-            if(newWindowSize % 2 == 1):
-                newWindowSize += 1
-            iIter = 0
-            while(newWindowSize != windowSize and iIter < self.maxIter):
-                windowSize = newWindowSize
-                frame = getFrame(x, iCenter, windowSize)
-                if(self.removeDC):
-                    frame = removeDCSimple(frame)
-                
-                buff = yin.difference(frame)
-                buff = yin.cumulativeDifference(buff)
-                valleyIndexList = yin.findValleys(buff, self.minFreq, self.maxFreq, self.samprate, threshold = self.valleyThreshold, step = self.valleyStep)
-                nValley = len(valleyIndexList)
-                if(valleyIndexList):
-                    possibleFreq = min(self.maxFreq, max(self.samprate / valleyIndexList[-1] - 20.0, self.minFreq))
-                    newWindowSize = int(max(self.samprate / possibleFreq * 4, self.minWindowSize) * self.windowLengthFac)
-                    if(newWindowSize % 2 == 1):
-                        newWindowSize += 1
-                    iIter += 1
-
-            freqProb = np.zeros((nValley, 2), dtype = np.float64)
-            probTotal = 0.0
-            weightedProbTotal = 0.0
-            for iValley, valley in enumerate(valleyIndexList):
-                ipledIdx, ipledVal = parabolicInterpolate(buff, valley)
-                freq = self.samprate / ipledIdx
-                v0 = 1 if(iValley == 0) else min(1.0, buff[valleyIndexList[iValley - 1]] + eps)
-                v1 = 0 if(iValley == nValley - 1) else max(0.0, buff[valleyIndexList[iValley + 1]]) + eps
-                prob = 0.0
-                for i in range(int(v1 * pdfSize), int(v0 * pdfSize)):
-                    prob += self.pdf[i] * (1.0 if(ipledVal < i / pdfSize) else 0.01)
-                prob = min(prob, 0.99)
-                prob *= self.bias
-                probTotal += prob
-                if(ipledVal < self.probThreshold):
-                    prob *= self.weightPrior
-                weightedProbTotal += prob
-                freqProb[iValley] = freq, prob
-
-            # renormalize
-            if(nValley > 0 and weightedProbTotal != 0.0):
-                freqProb.T[1] *= probTotal / weightedProbTotal
-
-            out.append(freqProb)
-
+        
+        out = np.zeros((nHop, self.maxFreqProbCount, 2), dtype = np.float32)
+        if self.useAccelerator:
+            try:
+                from . import accelerator
+                accelerator.pyinCore(x, self.pdf, self.bias, self.probThreshold, self.weightPrior, self.hopSize, self.valleyThreshold, self.valleyStep, self.minFreq, self.maxFreq, self.minWindowSize, self.windowLengthFac, self.samprate, self.maxIter, self.removeDC, out)
+            except Exception as e:
+                print("[ERROR] Failed to call accelerator, fallback: %s" % (str(e),))
+                _pyinCore(x, self.pdf, self.bias, self.probThreshold, self.weightPrior, self.hopSize, self.valleyThreshold, self.valleyStep, self.minFreq, self.maxFreq, self.minWindowSize, self.windowLengthFac, self.samprate, self.maxIter, self.removeDC, out)
+        else:
+            _pyinCore(x, self.pdf, self.bias, self.probThreshold, self.weightPrior, self.hopSize, self.valleyThreshold, self.valleyStep, self.minFreq, self.maxFreq, self.minWindowSize, self.windowLengthFac, self.samprate, self.maxIter, self.removeDC, out)
         return out
