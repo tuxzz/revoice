8c21cbe515639283a5b86a3b36de35c2836987a9
otonashi1991 <xir.hikaria@gmail.com>
2019-04-01 12:26:28 +0800

    stft

---
+++ script/stft.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append("../")
+
+from revoice import *
+from revoice.common import *
+
+import numpy as np
+import pylab as pl
+
+w, sr = loadWav("../voices/yuri_orig.wav")
+
+b, a = dc_iir_filter(50.0 / sr / 2.0)
+w = sp.filtfilt(b, a, w).astype(np.float32)
+
+window = nuttall(2048, nuttall_min3_coeff, sym=False)
+hop_size = 128
+assert sp.check_COLA(window, window.size, window.size - hop_size)
+assert sp.check_NOLA(window, window.size, window.size - hop_size)
+spec = stft(w, window, hop_size, 4096)
+reconstruct_w = istft(spec, istft_window(window, hop_size), hop_size, w.shape[0])
+
+pl.figure()
+pl.plot(w)
+pl.plot(reconstruct_w)
+pl.figure()
+pl.imshow(np.log(np.abs(spec) + 1e-5).T, interpolation='nearest', aspect='auto', origin='lower', vmin=(-80.0 / 20.0) / np.log10(np.e), vmax=(0.0 / 20.0) / np.log10(np.e))
+pl.show()
